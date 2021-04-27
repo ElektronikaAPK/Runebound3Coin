99d21b8b40aa4a3fa1e5e73a59f42b598f860744
kamilgrefkowicz <kamilgrefkowicz@gmail.com>
2021-04-27 19:36:18 +0200

    improved language selection menu

--- app/src/main/java/com/example/runebound3coin/MainActivity.java
+++ app/src/main/java/com/example/runebound3coin/MainActivity.java
@@ -4,19 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.runebound3coin.language.LanguageConfiguration;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup languageSelector;
+    RadioButton choseEnglish;
+    RadioButton chosePolish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        choseEnglish = findViewById(R.id.langChoiceEn);
+        chosePolish = findViewById(R.id.langChoicePl);
+        languageSelector = findViewById(R.id.radioGroupForLangSelection);
+
     }
 
 
     public void changeLanguage(View view) {
-        LanguageConfiguration.switchLocal(this, "pl");
+        String languageCode = Locale.getDefault().getCountry();
+        if (choseEnglish.isChecked()) languageCode = "en";
+        if (chosePolish.isChecked()) languageCode = "pl";
+        LanguageConfiguration.switchLocal(this, languageCode);
+        hideChangeLanguageRadio();
+    }
+
+    private void hideChangeLanguageRadio() {
+        languageSelector.setVisibility(View.INVISIBLE);
+    }
+
+    public void showChangeLanguageRadio(View view) {
+        languageSelector.setVisibility(View.VISIBLE);
     }
 }
